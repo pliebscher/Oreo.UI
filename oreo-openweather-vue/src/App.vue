930be5656b3b62ba5cf555003809825c11c35024
Philip Liebscher <11465292+pliebscher@users.noreply.github.com>
2022-10-25 14:16:58 -0700

    Updated App.vue

--- oreo-openweather-vue/src/App.vue
+++ oreo-openweather-vue/src/App.vue
@@ -1,19 +1,16 @@
 <script setup lang="ts">
-import HelloWorld from './components/Location.vue'
-import TheWelcome from './components/Weather.vue'
+import Location from './components/Location.vue'
+import Weather from './components/Weather.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <Location msg="You did it!" />
     </div>
   </header>
-
   <main>
-    <TheWelcome />
+    <Weather />
   </main>
 </template>
 
@@ -36,10 +33,6 @@ header {
     display: flex;
     place-items: center;
     padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
   }
 
   header .wrapper {
