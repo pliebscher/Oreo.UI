eba26684888d5a348e7c8b6525eb4e49ea2fbb4f
Philip Liebscher <11465292+pliebscher@users.noreply.github.com>
2022-10-31 11:47:31 -0700

    Fixed update() loop in Weather SFC

--- oreo-openweather-vue/src/components/Weather.vue
+++ oreo-openweather-vue/src/components/Weather.vue
@@ -6,12 +6,8 @@ import axios from 'axios'
 export default defineComponent({
         name: 'Weather',
         props:{
-            // TODO: prop validation
             lat: String,
             lon: String
-        },
-        setup() {
-
         },
         data() {
             return {
@@ -19,34 +15,49 @@ export default defineComponent({
                 metrics: {} as Metrics,
                 weather: {} as Weather,
                 weatherIconUrl: "01n", // https://openweathermap.org/weather-conditions#Icon-list
+                currentLat: "",
+                currentLon: ""
             }
         },
         methods: {
             async fetchWeather(lat?: string, lon?: string) {
                 // TODO: Axios error handling
                 // TODO: Remove hard-coded API hostname:port
-                const weatherResponse = await axios.get<WeatherResponse>('http://localhost:36416/api/Weather?Lat=' + lat + '&Lon=' + lon)
-                this.city = weatherResponse.data.city
-                this.metrics = weatherResponse.data.forecast[0].metrics
-                this.weather = weatherResponse.data.forecast[0].weather[0]
-                this.weatherIconUrl = weatherResponse.data.forecast[0].weather[0].icon
+                if (lat && lon) {
+                    console.info('fetching weather: ' + lat + ', ' + lon)
+                    const weatherResponse = await axios.get<WeatherResponse>('http://localhost:36416/api/Weather?Lat=' + lat + '&Lon=' + lon)
+                    this.city = weatherResponse.data.city
+                    this.metrics = weatherResponse.data.forecast[0].metrics
+                    this.weather = weatherResponse.data.forecast[0].weather[0]
+                    this.weatherIconUrl = weatherResponse.data.forecast[0].weather[0].icon
+                    this.currentLat = lat
+                    this.currentLon = lon
+                }
             },
             getWeatherIconUrl() {
                 return 'http://openweathermap.org/img/wn/' + this.weatherIconUrl + '@2x.png';
             }
         },
         async mounted() {
+            console.info('Weather.mounted(): ' + this.lat + ', ' + this.lon)
             await this.fetchWeather(this.lat, this.lon)
+        },
+        async updated() {
+            if (this.lat != this.currentLat) {
+                console.info('Weather.updated()')
+                await this.fetchWeather(this.lat, this.lon)
+            }
+            
         }
     })
 </script>
 
 <template>
     <div class="weather">
-        <h2>{{city.name}}</h2>
+        <h2>{{city?.name}}</h2>
         <img v-bind:src='getWeatherIconUrl()' />
-        <h2>{{metrics.temp}}&deg;</h2>
-        <div>{{weather.description}}</div>
+        <h2>{{metrics?.temp}}&deg;</h2>
+        <div>{{weather?.description}}</div>
         <div>Lat: {{lat}} Lon: {{lon}}</div>
     </div>
 </template>
