0de61545d23ed6e4842ff5ad35a3f76d011b109f
Philip Liebscher <11465292+pliebscher@users.noreply.github.com>
2022-11-05 11:09:22 -0700

    Pass Location object to Weather SFC

--- oreo-openweather-vue/src/components/Location.vue
+++ oreo-openweather-vue/src/components/Location.vue
@@ -1,7 +1,7 @@
 <script lang="ts">
 
 import { defineComponent, type StyleValue} from 'vue'
-import axios from 'axios'
+import axios, { type AxiosResponse } from 'axios'
 
 export default defineComponent({
         name: 'Location',
@@ -13,22 +13,22 @@ export default defineComponent({
                 errorVis: false,
             }
         },
+        emits:{
+            onLocationChanged(location: Location) { console.info('---> ' + location); return true }
+        },
         methods: {
             // TODO: Move to Axios module and;
             // TODO: Axios error handling
             // TODO: Remove hard-coded API hostname:port
             async fetchLocations(city?: string, state?: string, country?: string) {
+                const locationResponse = await axios.get('http://localhost:36416/api/GeoLocation?City=' + city +'&State=' + (state ? state : '') +'&Country=' + (country ? country : 'US'))
+                this.locations = locationResponse.data 
+            },
+            async getLocations() {
 
-                const locationResponse = await axios.get<Location[]>('http://localhost:36416/api/GeoLocation?City=' + city +'&State=' + (state ? state : '') +'&Country=' + (country ? country : 'US'))
-                this.locations = locationResponse.data
-             },
-             async getLocations() {
-
-                // Parse location search string...
+                // Parse location search string... TODO: Do this better!
                 if (this.searchStr.length > 0) {
-
                     var locArr = this.searchStr.split(/[\s,]+/);
-
                     if (locArr.length > 0)
                         await this.fetchLocations(locArr[0], locArr[1], 'us')
                 }
@@ -41,20 +41,18 @@ export default defineComponent({
                         this.locationsVis = true
                     }
                     this.errorVis = false
-                } else {
+                } else { // Location(s) Not Found...
                     this.locationsVis = false
                     this.errorVis = true
-                    //this.$emit("onLocationChanged", 0, 0)
                 }
-             },
+            },
             async selectLocation(location: Location) {
-                    console.info("Location.onLocationChanged: " + location.lat.toString() + ', ' + location.lon.toString())
-                    // TODO: Pass full Location object...
+                    console.info("Location.selectLocation: " + location.toString())
                     this.$emit("onLocationChanged", location)
             }
         },
         async mounted() {
-
+            //this.getLocations()
         }
     })
 </script>
@@ -69,8 +67,8 @@ export default defineComponent({
         <div v-if="locations?.length > 1">
             <h3>Found {{locations?.length}} Locations</h3>
             <hr />
-            <div v-for="loc in locations">
-            <a @click="selectLocation(loc)" href="#">{{loc.name}}</a>, {{loc.state}}
+            <div v-for="loc in locations.values()">
+            <a @click="selectLocation(loc)" href="#">{{loc.name}}</a>, {{loc.state}}, {{loc.toString()}}
         </div>
         </div>
 
