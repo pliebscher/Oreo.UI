e1cac879a923342e06cc65999e6ef0e8444eebef
Philip Liebscher <11465292+pliebscher@users.noreply.github.com>
2022-11-09 14:34:13 -0800

    Updated weather service

--- oreo-openweather-vue/src/services/weather.ts
+++ oreo-openweather-vue/src/services/weather.ts
@@ -7,20 +7,19 @@ const axiosClient = axios.create({
       'Accept': 'application/json',
       'Content-Type': 'application/json'
     }
-  });
+});
 
-  // TODO: Handle Axios errors...
-export default async function getLocations(city?: string, state?: string, country?: string) {
+// TODO: Handle Axios errors...
+
+export async function getLocations(city?: string, state?: string, country?: string) {
     const query = "GeoLocation?City=" + city + "&State=" + (state ? state : "") + "&Country=" + (country ? country : "US")
     const locationResponse = await axiosClient.get<GeoLocation[]>(query)
     return locationResponse.data
 }
 
 export async function getWeather(location?: GeoLocation) {
-
   if (location?.lat && location?.lon) {
-      console.info('fetchWeather: ' + location.lat + ', ' + location.lon)
-      const weatherResponse = await axios.get<WeatherResponse>('http://localhost:36416/api/Weather?Lat=' + location.lat + '&Lon=' + location.lon)
+      const weatherResponse = await axiosClient.get<WeatherResponse>('Weather?Lat=' + location.lat + '&Lon=' + location.lon)
       return weatherResponse.data
   }
 }
