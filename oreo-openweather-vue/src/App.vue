2bb53ac5029985be2183df37355a95fc2e73d3f8
Philip Liebscher <11465292+pliebscher@users.noreply.github.com>
2022-12-14 16:25:19 -0800

    Updated header

--- oreo-openweather-vue/src/App.vue
+++ oreo-openweather-vue/src/App.vue
@@ -43,19 +43,16 @@ export default defineComponent({
 
 <template>
   <!-- NavBar -->            
-  <div class="fixed bg-gradient-to-r from-gray-900 to-blue-700 shadow-md z-50 w-full px-5 py-2 ">
-      Weather
+  <div class="fixed bg-gradient-to-r from-gray-900 to-blue-700 shadow-md z-50 w-full px-4 py-1 text-lg">
+    🐕 Weather Dog
   </div>
   <!-- Content -->
-  <div class="mt-10 ml-0" >
-    <div class="">
+  <div class="w-auto mt-10" ></div>
+    <div class="w-full">
       <Location @onLocationChanged='locationChanged' />
       <Favorites @onLocationChanged='locationChanged' />
-      <div id="toaster" class="border rounded bg-red-900"></div>
-    </div>
-    <div class="">
       <Weather v-if="location" v-bind:location='location' />
       <Forecast v-if="location" v-bind:location='location' />    
     </div>
-  </div>
+    <div id="toaster" class="border rounded bg-red-900"></div>  
 </template>
