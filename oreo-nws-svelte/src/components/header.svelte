2d184eaa02510e2bcb3826e3f74277c67797f673
Philip Liebscher <11465292+pliebscher@users.noreply.github.com>
2023-01-13 11:47:55 -0800

    Updated about links

--- oreo-nws-svelte/src/components/header.svelte
+++ oreo-nws-svelte/src/components/header.svelte
@@ -3,13 +3,18 @@
 
 </script>
 
-<div class=" -mt-0 fixed bg-gradient-to-r from-gray-900 to-blue-700 shadow-md z-50 w-full pl-2 py-0 text-lg">
+<div class="-mt-0 fixed bg-gradient-to-r from-gray-900 to-blue-700 shadow-md z-50 w-full pl-2 py-0 text-lg">
 	<table class="w-full">
 		<tr>
-			<td class="">🐕 Weather Dog</td>
+			<td>
+                <a href="/" class="mb-1">
+                    🐕 
+                </a>
+                Weather Dog
+            </td>
 			<td class="justify-items-end" align="right">
-				<a href="https://svelte.dev/">
-					<img src="svelte.png" alt="Svelte" class="" width="25px" height="25px" />
+				<a href="/about">
+                    <img src="about.png" alt="About" aria-label="About" class="mr-1" width="23px" height="23px" />
 				</a>
 			</td>
 		</tr>
--- oreo-nws-svelte/src/routes/about/+page.svelte
+++ oreo-nws-svelte/src/routes/about/+page.svelte
@@ -1,9 +1,47 @@
 <script lang="ts">
 	import Header from "../../components/header.svelte";
 
-
-
-
 </script>
 
 <Header />
+
+<div class="mt-10"> 
+      <table class="ml-2">
+        <tr>
+            <td class="p-1">
+                <a href="https://svelte.dev/" aria-label="Svelte">
+                    <img src="svelte.png" alt="Svelte" aria-label="Svelte" class="mr-1" width="23px" height="23px" />
+                </a>
+            </td>
+            <td class="">
+                <a href="https://svelte.dev/" aria-label="Svelte">
+                    Svelte + SvelteKit
+                </a>
+            </td>
+        </tr>
+        <tr>
+            <td class="p-1">
+                <a href="https://www.weather.gov/" aria-label="National Weather Service">
+                    <img src="nws.png" alt="National Weather Service" aria-label="National Weather Service" class="mr-1" width="23px" height="23px" />
+                </a>
+            </td>
+            <td>
+                <a href="https://www.weather.gov/">
+                    National Weather Service
+                </a>
+            </td>
+        </tr>
+        <tr>
+            <td class="p-1">
+                <a href="https://github.com/pliebscher/Oreo.UI/tree/master/oreo-nws-svelte" aria-label="GitHub">
+                    <img src="GitHub.png" alt="Svelte" aria-label="GitHub" class="mr-1" width="23px" height="23px" />
+                </a>
+            </td>
+            <td>
+                <a href="https://github.com/pliebscher/Oreo.UI/tree/master/oreo-nws-svelte" aria-label="GitHub">
+                    GitHub Repository
+                </a>
+            </td>
+        </tr>
+      </table>
+</div>
