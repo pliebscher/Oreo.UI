13da14e001e36ad6dc155ade3ea79feb5cc10a3b
Philip Liebscher <11465292+pliebscher@users.noreply.github.com>
2023-05-28 20:20:11 -0700

    Added footer copy

---
+++ oreo-nws-svelte/src/components/footer.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import Container from "./container.svelte";
+
+
+
+</script>
+
+<Container id="footer">
+    <div>
+        &copy; 2023 Weather Fish
+    </div>
+</Container>
--- oreo-nws-svelte/src/routes/+page.svelte
+++ oreo-nws-svelte/src/routes/+page.svelte
@@ -3,6 +3,7 @@
 	import LocationSearch from "../components/locationSearch.svelte";
 	import Favorites from "../components/favorites.svelte";
 	import Weatherforecast from "../components/weatherForecast.svelte";
+	import Footer from "../components/footer.svelte";
 
 	import type { arcGISLocation } from "src/models/arcGISLocation";
 
@@ -17,5 +18,6 @@
 <LocationSearch on:locationSelected = {onLocationSelected} />
 <Favorites on:locationSelected = {onLocationSelected} />
 <Weatherforecast location = {selectedLocation} />
+<Footer />
 
 
