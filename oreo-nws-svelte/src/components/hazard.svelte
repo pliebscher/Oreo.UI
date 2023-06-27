bef91a0031126fb6441d9160e3c741a726a990da
Philip Liebscher <11465292+pliebscher@users.noreply.github.com>
2023-06-27 14:27:23 -0700

    Added detail link to hazard module

--- oreo-nws-svelte/src/components/hazard.svelte
+++ oreo-nws-svelte/src/components/hazard.svelte
@@ -8,13 +8,13 @@
 
 {#if forecast?.data.hazard[0]}
 <Container id="hazard">
-    <table class="w-full">
+    <table class="w-full rounded  p-2 bg-gradient-to-r from-gray-900 to-orange-500">
         <tr>
-            <td class="w-7">
-                <img src="/images/warning.png" alt="Tools" aria-label="Tools" class=" w-5" />
+            <td class="w-7 align-top">
+                <img src="/images/warning.png" alt="Tools" aria-label="Tools" class="w-6 align-top" />
             </td>
             <td class="">
-                {forecast?.data.hazard[0]}
+                {forecast?.data.hazard[0]}... [<a href="{forecast?.data.hazardUrl[0].replace('amp;', '')}" target="_blank" rel="noreferrer">Detail</a>]
             </td>
         </tr>
     </table>
